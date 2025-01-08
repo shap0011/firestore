d6c970001c66f54087703604c294bb5281350c04
shap0011 <shap0011@algonquinlive.com>
2025-01-08 02:48:34 -0500

    edits code

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     <>
       <ul>
         {contacts ? (
-          contacts.map((contact)=><li key={contact.id}>{contact.name}</li>)
+          contacts.map((contact)=><li key={contact.id}>{contactName}</li>)
         ) : (
           <p>Loading...</p>
         )}
