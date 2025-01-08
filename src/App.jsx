bb53f72f46f427fad72605510e5c42ef1e39e756
shap0011 <shap0011@algonquinlive.com>
2025-01-08 02:26:05 -0500

    edits code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css';
 import { collection, getDocs } from 'firebase/firestore';
 import db, { CONTACTS_DATABASE_ID } from './db';
@@ -12,21 +10,12 @@ function App() {
     const querySnapshot = await getDocs(collection(db, CONTACTS_DATABASE_ID));
     querySnapshot.forEach((doc)=>{
       const data = doc.data();
-      console.log(doc.id);
-      console.log(data);
+      // console.log(doc.id);
+      // console.log(data);
       contactList.push({
         id:doc.id,
         ...data,
       });
-      // [
-      //   {
-      //     id: "hhuhudeurfgtij56b3v",
-      //     name: "John",
-      //     lastName: "Doe",
-      //     eMail: "jd!@jd.com",
-      //     age: 30,
-      //   }
-      // ]
     });
     setContacts(contactList);
   };
