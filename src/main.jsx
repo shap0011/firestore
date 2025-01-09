498b8fd45f875507a26b6a0618f7d7e6cd4ad333
shap0011 <shap0011@algonquinlive.com>
2025-01-09 16:43:03 -0500

    edits code

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <App />
   },
   {
-    path:"firestore/pages/details/:id",
+    path:"/firestore/details/:id",
     element: <Details />
   },
 ]);
