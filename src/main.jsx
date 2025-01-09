e3815360ecdc9cd4bcb548777939278ffe64412d
shap0011 <shap0011@algonquinlive.com>
2025-01-09 15:49:40 -0500

    edits code

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './index.css';
+
 // import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 // const router = createBrowserRouter(
