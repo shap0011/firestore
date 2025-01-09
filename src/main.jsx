2c176d4b0da38eebb81c9b821d689c7e0c0740a5
shap0011 <shap0011@algonquinlive.com>
2025-01-09 15:46:28 -0500

    edits code

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,29 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+// import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-const router = createBrowserRouter(
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path:"/details/:id",
-    element: <Details />
-  }
-)
+// const router = createBrowserRouter(
+//   {
+//     path: "/",
+//     element: <App />
+//   },
+//   {
+//     path:"/details/:id",
+//     element: <Details />
+//   }
+// )
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router}>
+//       <App />
+//     </RouterProvider>
+//   </React.StrictMode>,
+// )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
       <App />
-    </RouterProvider>
   </React.StrictMode>,
 )
