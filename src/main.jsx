6bf8a94dd548952b2fa356948a1684db409c4625
shap0011 <shap0011@algonquinlive.com>
2025-01-09 11:08:39 -0500

    adds Details.jsx and details.css files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter(
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path:"/details/:id",
+    element: <Details />
+  }
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </React.StrictMode>,
 )
