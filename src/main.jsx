e4fffd5e51430de69d16da954c1070b093402152
shap0011 <shap0011@algonquinlive.com>
2025-01-09 12:01:17 -0500

    edits code

--- src/main.jsx
+++ src/main.jsx
@@ -18,8 +18,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <RouterProvider router={router} />
+      {/* <App />
+    </RouterProvider> */}
   </React.StrictMode>,
 )
