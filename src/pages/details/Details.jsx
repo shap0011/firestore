ef9f09ccc42bfa433058cffa2eb124678ccdb481
shap0011 <shap0011@algonquinlive.com>
2025-01-10 01:26:48 -0500

    edits Details.jsx

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,12 +4,14 @@ import db, { CONTACTS_DATABASE_ID } from "../../db";
 import { useParams } from "react-router-dom";
 
 const Details = () => {
+    const [contact, setContact] = useState();
     const { id } = useParams();
     const getContact = async()=>{
         const docRef = doc(db, CONTACTS_DATABASE_ID, id);
         const docSnap = await getDoc(docRef);
         if(docSnap.exists()){
             console.log("Contact", docSnap.data());
+            setContact(docSnap.data());
         }else{
             console.log("No document with id", id, "found");
         }
@@ -17,7 +19,19 @@ const Details = () => {
     useEffect(()=>{
         getContact();   
     },[])
-    return <h1>Details</h1>
+    return <div>
+        <h6>
+            Contact Details:
+        </h6>
+        {contact && (<div>
+            <p>{contact.firstName}</p>
+            <p>{contact.lastName}</p>
+            <p>{contact.email}</p>
+            <p>{contact.phone}</p>
+            <p>{contact.age}</p>
+        </div>)}
+        
+        </div>
 }
 
 export default Details;
