b679d66e5f8bc09c0a0133beddbed590d8815c50
shap0011 <shap0011@algonquinlive.com>
2025-01-09 20:53:07 -0500

    edits App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     <>
       <ul className='list'>
         {contacts ? (
-          contacts.map((contact)=><Link to={`/details/${contact.id}`} key={contact.id}>{contact.name}</Link>)
+          contacts.map((contact)=><Link to={`/details/${contact.id}`} key={contact.id}>{contact.lastName}</Link>)
         ) : (
           <p>Loading...</p>
         )}
