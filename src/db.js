aca8a371dcb7167a58cb4909874b17f0dfa7d387
shap0011 <shap0011@algonquinlive.com>
2025-01-09 20:24:57 -0500

    adds new db

--- src/db.js
+++ src/db.js
@@ -6,13 +6,23 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC3m93k1YI4by_mmA9E4QQsVIcCa_w5Qng",
-  authDomain: "winter-2025.firebaseapp.com",
-  projectId: "winter-2025",
-  storageBucket: "winter-2025.firebasestorage.app",
-  messagingSenderId: "573011371463",
-  appId: "1:573011371463:web:ad7491d29fda24f10ea5e2"
+  apiKey: "AIzaSyDNtgwaVRdIUCyPzWkNrMcQy2FYOse_Kf4",
+  authDomain: "fir-summer-2024-ce6f0.firebaseapp.com",
+  projectId: "fir-summer-2024-ce6f0",
+  storageBucket: "fir-summer-2024-ce6f0.appspot.com",
+  messagingSenderId: "380846268982",
+  appId: "1:380846268982:web:7a168fdc8071fda8306e97"
 };
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC3m93k1YI4by_mmA9E4QQsVIcCa_w5Qng",
+//   authDomain: "winter-2025.firebaseapp.com",
+//   projectId: "winter-2025",
+//   storageBucket: "winter-2025.firebasestorage.app",
+//   messagingSenderId: "573011371463",
+//   appId: "1:573011371463:web:ad7491d29fda24f10ea5e2"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
