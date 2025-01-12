74d653cbd0d0ca6b6b695cb72a7234be524525a4
shap0011 <shap0011@algonquinlive.com>
2025-01-12 14:16:43 -0500

    edits modal

--- src/components/contactForm/contactFormModal.jsx
+++ src/components/contactForm/contactFormModal.jsx
@@ -3,28 +3,26 @@ const Modal = ({open}) => {
     return open ? (
         <div className = "container">
             <div className = "modal">
-                <div>
-                    <label>
-                        First Name
-                        <input type="text" name="firstName" />
-                    </label>
-                    <label>
-                        Last Name
-                        <input type="text" name="lastName" />
-                    </label>
-                    <label>
-                        E-mail
-                        <input type="text" name="email" />
-                    </label>
-                    <label>
-                        Phone
-                        <input type="text" name="phone" />
-                    </label>
-                    <label>
-                        Age
-                        <input type="text" name="age" />
-                    </label>
-                </div>
+                <label>
+                    First Name
+                    <input type="text" name="firstName" />
+                </label>
+                <label>
+                    Last Name
+                    <input type="text" name="lastName" />
+                </label>
+                <label>
+                    E-mail
+                    <input type="text" name="email" />
+                </label>
+                <label>
+                    Phone
+                    <input type="text" name="phone" />
+                </label>
+                <label>
+                    Age
+                    <input type="text" name="age" />
+                </label>
             </div>
         </div> 
     ) : null;
